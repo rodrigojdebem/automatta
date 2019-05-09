eda4bdb75b84a820edf96160d495fdb41e5599a4
rodrigodebem1 <rodrigo@simbioseventures.com>
2019-05-09 04:23:23 -0300

    Updates

---
+++ precache-manifest.9b8e0a9e24cc0a9af8e75e2a58413e16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34821e4b2bbc136283ccbe3385b08da3",
+    "url": "/automatta/index.html"
+  },
+  {
+    "revision": "938d92fdffc8219db14a",
+    "url": "/automatta/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/automatta/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "938d92fdffc8219db14a",
+    "url": "/automatta/static/js/main.c8cff267.chunk.js"
+  },
+  {
+    "revision": "4719797a46e61a9eaf53",
+    "url": "/automatta/static/js/runtime~main.a8b6a277.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/automatta/static/media/logo.5d5d9eef.svg"
+  }
+]);
